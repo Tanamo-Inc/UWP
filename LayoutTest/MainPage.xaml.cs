57acb8c3f0521db284555bac0d5c004b2a52ef00
Unknown <tanamoinc@gmail.com>
2018-07-28 22:42:30 +0100

    commit

--- LayoutTest/MainPage.xaml.cs
+++ LayoutTest/MainPage.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace HelloWorld
+namespace LayoutTest
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -26,21 +26,5 @@ namespace HelloWorld
         {
             this.InitializeComponent();
         }
-
-
-        // XAML - XML Syntax, create instances of Classes that define the UI.
-       // XAML is a declarative markup language.
-
-
-
-
-
-        // Button Click Method.
-        private void ClickBut_Click(object sender, RoutedEventArgs e)
-        {
-            display.Text = "Hello World";
-
-        }
-
     }
 }
