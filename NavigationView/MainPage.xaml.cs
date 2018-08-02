382f133c501c7aee626c225d4d2c0eb6885a93df
Unknown <tanamoinc@gmail.com>
2018-08-02 08:34:20 +0100

    commit

---
+++ NavigationView/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace NavigationView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            HomeFrame.Navigate(typeof(Page1));
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            HomeFrame.Navigate(typeof(Page1)); 
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (HomeFrame.CanGoBack)
+            {
+                HomeFrame.GoBack();
+            }
+
+        }
+
+        private void ForwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (HomeFrame.CanGoForward)
+            {
+                HomeFrame.GoForward();
+            }
+        }
+
+        private void NavigateButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Page2));
+
+        }
+    }
+}
