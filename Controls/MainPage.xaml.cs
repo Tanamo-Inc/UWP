b243474bc51c148689acc48a68d8270a5a878764
Unknown <tanamoinc@gmail.com>
2018-08-13 20:48:19 +0100

    commit

---
+++ Controls/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Controls
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+        //CheckBox Method
+        private void checker_tap(object sender, TappedRoutedEventArgs e)
+        {
+            disp_checker.Text = checker.IsChecked.ToString();
+        }
+
+        //RadioButton Method
+        private void checker_radio(object sender, RoutedEventArgs e)
+        {
+            disp_radio.Text = (bool)YesRadioButton.IsChecked ? "Yes" : "No";
+        }
+
+        //ComboBox Method
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (disp_combo == null) return;
+
+            var combo = (ComboBox)sender;
+            var item = (ComboBoxItem)combo.SelectedItem;
+
+            disp_combo.Text = item.Content.ToString();
+
+        }
+
+        //ListBox Method
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedItems = MyListBox.Items.Cast<ListBoxItem>()
+                                  .Where(p => p.IsSelected)
+                                    .Select(t => t.Content.ToString())
+                                      .ToArray();
+
+            disp_list.Text = string.Join(", ", selectedItems);
+
+        }
+
+        //ToggleButton Method
+        private void MyToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            disp_toggle.Text = MyToggleButton.IsChecked.ToString();
+        }
+
+
+    }
+}
---
+++ NavigationBar/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace NavigationBar
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        // Navigation Button Method.
+        private void Nav_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+   
+        //Navigation Listener Method.
+        private void nav_view_listener(object sender, SelectionChangedEventArgs e)
+        {
+            if (share_item.IsSelected) { disp.Text = "Share"; }
+            else if (fav_tem.IsSelected) { disp.Text = "Favorites"; }
+        }
+
+    }
+}
