fe004e4e451d12feb20a6a96cc18fbc2dbf67a85
Unknown <tanamoinc@gmail.com>
2018-08-15 09:57:50 +0100

    commit

--- Controls2/App.xaml.cs
+++ Controls2/App.xaml.cs
@@ -5,7 +5,7 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-namespace HelloWorld
+namespace Controls2
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
---
+++ Controls2/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Controls2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private string[] arra = new string[] { "Anthony", "Antoinette", "Anita", "Albert", "Alberta", "Bernice", "Bernard", "Charcles" };
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MyCalendarView_SelectedDatesChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
+        {
+            var selectedDates = sender.SelectedDates.Select(p => p.Date.Month.ToString() + "/" + p.Date.Day.ToString()).ToArray();
+            var values = string.Join(", ", selectedDates);
+            CalendarViewResultTextBlock.Text = values;
+
+        }
+
+        private void InnerFlyoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyFlyout.Hide();
+        }
+
+   
+        private void MyAutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            var autoSuggestBox = (AutoSuggestBox)sender;
+            var filtered = arra.Where(p => p.StartsWith(autoSuggestBox.Text)).ToArray();
+            autoSuggestBox.ItemsSource = filtered;
+        }
+
+
+
+    }
+
+
+}
